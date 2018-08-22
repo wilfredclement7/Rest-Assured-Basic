00cc884da25e3676e6a4ab42d101e303103be50b
wilfredclement7 <wilfredclement7@hotmail.com>
2018-08-22 17:18:56 +0530

    Update
    Update

--- src/test/java/alpha/Flow2_Hash.java
+++ src/test/java/alpha/Flow2_Hash.java
@@ -1,36 +1,34 @@
 package alpha;
 
-import org.json.simple.JSONObject;
+//import org.json.simple.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class Flow2 {
+public class Flow2_Hash {
 
-	@SuppressWarnings({ "unchecked", "serial" })
+	@SuppressWarnings("all")
     @Test
     public void TC_1() {
 
 		Map<String, Object> map = new HashMap<>();
-		map.put("Type", "Something");
-		map.put("Authentication", asList(new HashMap<String, Object>() {{
+		map.put("Type", "Nothing");
+		map.put("Authentication", Arrays.asList(new HashMap<String, Object>() {{
 		    put("Key", "key1");
 		    put("Value", "value1");
 		}}, new HashMap<String, Object>() {{
 		    put("Key", "key2");
 		    put("Value", "value2");
-		}}, new HashMap<String, Object>() {{
-		    put("Key", "key3");
-		    put("Value", "value3");
-		}}));		
-
+		}}
+		));
+		
 		RequestSpecification req=RestAssured.given();
 		req.header("Content-Type","application/json");
-		//req.body(json).when();
+		req.body(map).when();
 		Response resp = req.post("http://localhost:3000/posts");
 		
 		int code = resp.getStatusCode();
