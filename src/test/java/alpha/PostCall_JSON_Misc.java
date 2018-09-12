f9d57399c0deb7f2ab961976ff578c9b8957db81
wilfredclement7 <42040151+wilfredclement7@users.noreply.github.com>
2018-09-12 06:01:35 -0700

    Update PostCall_JSON_Misc.java

--- src/test/java/alpha/PostCall_JSON_Misc.java
+++ src/test/java/alpha/PostCall_JSON_Misc.java
@@ -10,34 +10,33 @@ import org.testng.Assert;
 
 public class PostCall_JSON_Misc {
 
-@SuppressWarnings("unchecked")
-@Test
-public void TC_2()
-	{
-		RequestSpecification req = RestAssured.given();
-		req.header("Content-Type","application/json");
-		
+    @SuppressWarnings("unchecked")
+    @Test
+    public void TC_2() {
+        RequestSpecification req = RestAssured.given();
+        req.header("Content-Type", "application/json");
+
         JSONObject requestparams = new JSONObject();
         JSONArray authArray = new JSONArray();
         JSONObject authparam = new JSONObject();
 
-            requestparams.put("Type", "Nothing");
+        requestparams.put("Type", "Nothing");
 
-            authparam.put("Value", "value1");
-            authparam.put("Key", "key1");
-	    authparam.put("Value", "value2");
-            authparam.put("Key", "key2");
-            authArray.add(authparam);
+        authparam.put("Value", "value1");
+        authparam.put("Key", "key1");
+        authparam.put("Value", "value2");
+        authparam.put("Key", "key2");
+        authArray.add(authparam);
 
-            requestparams.put("Authentication", authparam);
-		
-		req.body(requestparams.toJSONString());
-		Response resp = req.post("http://localhost:3000/posts");
-		
-		int code = resp.getStatusCode();
-		System.out.println("Status Code is : " + resp);
-		String body = resp.asString();
-		System.out.println("Body is : " + body);
-		Assert.assertEquals(code, 201);
-	}
+        requestparams.put("Authentication", authparam);
+
+        req.body(requestparams.toJSONString());
+        Response resp = req.post("http://localhost:3000/posts");
+
+        int code = resp.getStatusCode();
+        System.out.println("Status Code is : " + resp);
+        String body = resp.asString();
+        System.out.println("Body is : " + body);
+        Assert.assertEquals(code, 201);
+    }
 }
